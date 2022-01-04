55b14657722f6c73f7a5cfa401159bc77de34587
skogard <skogard@protonmail.com>
2022-01-04 08:31:34 -0500

    mainnet deploy & verify

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-//import 'hardhat/console.sol';
 import "@openzeppelin/contracts/proxy/Clones.sol";
 import "./Stream.sol";
 contract Factory {
   event ContractDeployed(address indexed owner, address indexed group, string title);
-  address immutable implementation;
+  address public immutable implementation;
   constructor() {
     implementation = address(new Stream());
   }
--- hardhat.config.js
+++ hardhat.config.js
@@ -19,6 +19,31 @@ task("deploy", "deploys the contract", async (args, hre) => {
   await fs.promises.writeFile(path.resolve(__dirname, `./deployments/${hre.network.name}.json`), JSON.stringify({ address: factory.address }))
   return factory;
 })
+task("v", "verify on etherscan", async (args, hre) => {
+  console.log("x")
+  const FACTORY_ABI = require(path.resolve(__dirname, "./abi/contracts/Factory.sol/Factory.json"));
+  const FACTORY_JSON = require(path.resolve(__dirname, `./deployments/${hre.network.name}.json`));
+  let factoryAddress = FACTORY_JSON.address
+  console.log("verify factory", factoryAddress)
+  try {
+    await hre.run("verify:verify", {
+      address: factoryAddress
+    });
+  } catch (e) {
+    console.log("already verified")
+  }
+  const [deployer] = await ethers.getSigners();
+  let contract = new ethers.Contract(factoryAddress, FACTORY_ABI, deployer)
+  let implementation = await contract.implementation()
+  console.log("verify implementation", implementation)
+  try {
+    await hre.run("verify:verify", {
+      address: implementation
+    })
+  } catch (e) {
+    console.log("already verified")
+  }
+})
 module.exports = {
   gasReporter: {
     currency: "USD",
@@ -53,6 +78,7 @@ module.exports = {
       accounts: [process.env.RINKEBY_PRIVATE_KEY],
     },
     mainnet: {
+      gasPrice: 77000000000,
       timeout: 1000 * 60 * 60 * 24, // 1 day
       url: process.env.MAINNET,
       accounts: [process.env.MAINNET_PRIVATE_KEY],
